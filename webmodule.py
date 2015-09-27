48dd56ed46c630b314f8d0ff0e75f01bfb8e2234
Shinseeker <dailymanj88@live.com>
2015-09-27 19:37:28 +0000

    webmodule example

---
+++ webmodule.py
@@ -0,0 +1,12 @@
+#!usr/bin/env python
+# -*- codig: utf-8 -*-
+
+import random
+import urllb.request
+
+def download_web_image(url):
+    name = random.randrange(1, 1000)
+    full_name + str(name) + ".jpg"
+    urllib.request.urlretreive(url, full_name)
+
+download_web_image(link)
